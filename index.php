b558c6fb178a8bb3c6e7ef102397e21659fd30c5
daniel <senyonjodaniel6@gamil.com>
2022-09-14 18:33:08 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <section class="sidebar">
+        <div>
+            <div style="width: 20px; height: 20px; padding: 5px; border-radius: 100%; background: rgb(36, 2, 41); fill: white;">
+                <span><?php include"vectors/bars.svg"?></span>
+            </div>
+        </div>
+        <div class="list">
+            <div style="display: flex;">
+                <div style="width: 20px; height: 20px; padding: 5px; border-radius: 100%; background: rgb(36, 2, 41); fill: white;">
+                    <span><?php include"vectors/circle-user.svg"?></span>
+                </div>
+                <a href="#">sign in</a>
+            </div>
+            <div class="flight">
+                <div style="width: 20px; height: 20px; padding: 5px; border-radius: 100%; background: rgb(36, 2, 41); fill: white;">
+                <span><?php include"vectors/plane-up.svg"?></span>
+                </div>
+                <a href="#">Flights</a>
+            </div>
+            <div style="display: flex;">
+                <div style="width: 20px; height: 20px; padding: 5px; border-radius: 100%; background: rgb(36, 2, 41); fill: white;">
+                    <span><?php include"vectors/bed.svg"?></span>
+                </div>
+                <a href="#">Stays</a>
+            </div>
+            <div style="display: flex;">
+                <div style="width: 20px; height: 20px; padding: 5px; border-radius: 100%; background: rgb(36, 2, 41); fill: white;">
+                    <span><?php include"vectors/car.svg"?></span>
+                </div> 
+                <a href="#">Car Rentals</a>
+            </div>
+            <div style="display: flex;">
+                <div style="width: 20px; height: 20px; padding: 5px; border-radius: 100%; background: rgb(36, 2, 41); fill: white;">
+                    <span><?php include"vectors/chair.svg"?></span>
+                </div>
+                <a href="#">Things to do</a>
+            </div>
+            <div style="display: flex;">
+                <div style="width: 20px; height: 20px; padding: 5px; border-radius: 100%; background: rgb(36, 2, 41); fill: white;">
+                    <span><?php include"vectors/umbrella-beach.svg"?></span>
+                </div>
+                <a href="#">Packages</a>
+            </div>
+            <div style="display: flex;">
+                <div style="width: 20px; height: 20px; padding: 5px; border-radius: 100%; background: rgb(36, 2, 41); fill: white;">
+                    <span><?php include"vectors/bus.svg"?></span>
+                </div>
+                <a href="#">Trains and buses</a>
+            </div>
+            <div class="explore">
+                <div style="width: 20px; height: 20px; padding: 5px; border-radius: 100%; background: rgb(36, 2, 41); fill: white;">
+                    <span><?php include"vectors/globe.svg"?></span>
+                </div>
+                <a href="#">Explore</a>
+            </div>
+            <div style="display: flex;">
+                <div style="width: 20px; height: 20px; padding: 5px; border-radius: 100%; background: rgb(36, 2, 41); fill: white;">
+                    <span><?php include"vectors/shield-heart.svg"?></span>
+                </div>
+                <a href="#">Travel Restrictions</a>
+            </div>
+            <div class="trip">
+                <div style="width: 20px; height: 20px; padding: 5px; border-radius: 100%; background: rgb(36, 2, 41); fill: white;">
+                    <span><?php include"vectors/book.svg"?></span>
+                </div>
+                <a href="#">Trips</a>
+            </div>
+           
+        </div>
+    </section>
+    <section class="main">
+        <div class="top">
+            <div>
+                <span style="color: blue;">mo</span>
+                <span style="color: tomato;">mon</span> 
+                <span style="color: hotpink;">do</span>
+            </div>
+            <div>Trips</div>
+            <div style="display: flex;">
+                <div style="width: 20px; height: 20px; padding: 5px; border-radius: 100%; background: rgb(36, 2, 41); fill: white;">
+                    <span style="width: 20px;"><?php include"vectors/circle-user.svg"?></span>
+                </div>
+                <a href="#">sign in</a>
+            </div>
+        </div>
+        <div class="para">
+            <h2>Find and compare cheap flights</h2>
+        </div>
+        <div class="rounds">
+            <div style="display: flex;">
+                Round-trip
+                <div style="width: 20px; height: 20px; padding: 5px; border-radius: 100%; background: rgb(36, 2, 41); fill: white;">
+                    <span style="width: 12px;"><?php include"vectors/angle-down.svg"?></span>
+                </div>
+            </div>
+            <div style="display: flex;">
+                1 adult
+                <div style="width: 20px; height: 20px; padding: 5px; border-radius: 100%; background: rgb(36, 2, 41); fill: white;">
+                        <span style="width: 12px;"><?php include"vectors/angle-down.svg"?></span>
+                    </div>
+            </div>
+            <div style="display: flex;">
+                Economy
+                <div style="width: 20px; height: 20px; padding: 5px; border-radius: 100%; background: rgb(36, 2, 41); fill: white;">
+                        <span style="width: 12px;"><?php include"vectors/angle-down.svg"?></span>
+                    </div>
+            </div>
+            <div style="display: flex;">
+                0 bags
+                <div style="width: 20px; height: 20px; padding: 5px; border-radius: 100%; background: rgb(36, 2, 41); fill: white;">
+                        <span style="width: 12px;"><?php include"vectors/angle-down.svg"?></span>
+                    </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="box1">
+                <div style="display: flex;">
+                    <div class="ebb">
+                        Entebbe(EBB)
+                        <div style="width: 16px; height: 16px; border-radius: 5px; margin: 5px; background: white; fill: #878767;">
+                            <span style="width: 11px;"><?php include"vectors/circle-xmark.svg"?></span>
+                       </div>
+                                 
+                   </div> 
+                  <div style="width: 30px; height: 20px; border-radius: 5px; background: white; margin-left: 5px;">
+                        <div style="width: 16px; height: 15px; border-radius: 5px; margin-left: 6px; fill: #878767;">
+                            <span style="width: 11px;"><?php include"vectors/plus.svg"?></span>
+                       </div>
+                  </div>
+                </div>
+                <div style="flex-direction: column; padding: 0px; background: #66dd66">
+                        <div style="width: 16px; height: 12px; border-radius: 5px; margin: px; background: ; fill: #878767;">
+                                <span style="width: 11px;"><?php include"vectors/arrow-right-long.svg"?></span>
+                            </div>
+                        <div style="width: 16px; height: 10px; border-radius: 5px; margin: px; background: ; fill: #878767;">
+                                <span style="width: 11px;"><?php include"vectors/arrow-left-long.svg"?></span>
+                            </div>
+                </div>
+                <div style="display: flex;">
+                    <div style="width: 20px; height: 20px; padding: 5px; border-radius: 5px; background: ; fill: #878767;">
+                    <span><?php include"vectors/plane-up.svg"?></span>
+                    </div>
+                    TO?
+                </div>
+                <div>
+                    <div style="display: flex;     align-items: center;">
+                        <div style="width: 16px; height: 20px; border-radius: 5px; padding: 6px; background: ; fill: #878767;">
+                                <span style="width: 11px;"><?php include"vectors/calendar-check.svg"?></span>
+                            </div>
+                        sat 10/1
+                    </div>
+                    
+                    <div style="display: flex;     align-items: center;">
+                        <div style="width: 16px; height: 20px; border-radius: 5px; padding: 6px; background: ; fill: #878767;">
+                                <span style="width: 11px;"><?php include"vectors/calendar-check.svg"?></span>
+                            </div>
+                        sat 10/8
+                    </div>
+                </div>
+            </div>
+            <div class="box2">Search</div>
+        </div>
+        <div class="title">
+           <h1>Trending cities</h1>
+           <p>The most seached for cities on momondo</p>
+        </div>
+        <div class="cities">
+            <div>
+                <div>
+                    <img src="images/dde6a0168a34d75a93aee002da5bc2f5.jpg" alt="">
+                    <p><span>FLIGHTS TO</span><br> Orlando</p>
+                </div>
+                <div>
+                    <img src="images/4af63d4fc18f1d57f6d1e990fd183901.jpg" alt="">
+                    <p><span>FLIGHTS TO</span><br> Lisbon</p>
+                </div>
+                <div>
+                    <img src="images/677d24a43bab53804e5a00477f367c23.jpg" alt="">
+                    <p><span>FLIGHTS TO</span><br> Cancun</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="images/84d43b0ca7fde4b6f3e153391be0ce66.jpg" alt="">
+                    <p><span>FLIGHTS TO</span><br> London</p>
+                </div>
+                <div>
+                    <img src="images/b6c10cd60797a5d79e4b5842da2d9130.jpg" alt="">
+                    <p><span>FLIGHTS TO</span><br>San Francisco</p>
+                </div>
+                <div>
+                    <img src="images/35851addd60ed25fa4a5811ab632ecf9.jpg" alt="">
+                    <p><span>FLIGHTS TO</span><br> Stockholm</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
